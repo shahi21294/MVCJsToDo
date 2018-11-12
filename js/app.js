8108fe14be2168bb6a54a4a5c942d46d6fc5449a
AliAkbar shahi <AliAkbar.Shahi@MahanAir.Aero>
2018-11-12 16:54:15 +0330

    remove view as args from controller

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 	 var PubSubTask = new PubSub();
 	 var model = new taskModel(PubSubTask);
 	 var view = new taskView(model,PubSubTask);
-	 var  controller = new taskController(model, view,PubSubTask);
+	 var  controller = new taskController(model,PubSubTask);
 })();
 		 
 		
